f83e9c33fd0a00c2c23f950a22da145ced765159
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-02-07 18:23:42 +0530

    Arrays & Functions

--- TypeScript.js
+++ TypeScript.js
@@ -1,7 +1,8 @@
 console.log("Hello TypeScript!");
 // variable types
 var a = 18;
-console.log(a); //>>>18
+console.log(a);
+//>>>18
 //let a = "Rutvi" 
 // It will show an error. Block scoped variables can't be declared 
 //again with different datatype.
@@ -12,8 +13,17 @@ var d = 100;
 var x;
 x = 100;
 console.log(">>> Any Type variable: x=", x, typeof x);
+// >>> Any Type variable: x= 100 number
 x = "Rutvi";
 console.log(">>> Any Type variable: x=", x, typeof x);
+// >>> Any Type variable: x= Rutvi string
+// If number is starting with 0 in typescript, then it will consider that number as an octal number, 
+// except the immediate number after 0 is 8 or 9. 
+// let y = 02.34 // this will also show an error because octal numbers must be integer.
+var y = "02.34";
+var z = parseFloat(y);
+console.log(">>> parse the string as a float", z);
+//>>> parse the string as a float 2.34
 // Enum
 var LanguagesKnown;
 (function (LanguagesKnown) {
@@ -23,13 +33,13 @@ var LanguagesKnown;
 })(LanguagesKnown || (LanguagesKnown = {}));
 // Arrays
 var students = {
-    Name: "Rutvi Patel",
+    Name: "Rutvi",
     Age: 21,
     Phone: 1234567890,
     Language: LanguagesKnown[LanguagesKnown.English]
 };
 var studentlist = [
-    { Name: "John", Age: 20, Phone: 1023456789, Language: LanguagesKnown[LanguagesKnown.Hindi] },
+    { Name: "John", Age: 20, Phone: 1023456798, Language: LanguagesKnown[LanguagesKnown.Hindi] },
     { Name: "Mark", Age: 22, Phone: 1203456789, Language: LanguagesKnown[LanguagesKnown.Tamil] }
 ];
 studentlist.push(students);
@@ -37,4 +47,66 @@ for (var index = 0; index < studentlist.length; index++) {
     var element = studentlist[index];
     console.log("Age: " + element.Age + " with Name " + element.Name + " has Phone: " + element.Phone + " knows language " + element.Language);
 }
+/*
+  Age: 20 with Name John has Phone: 1023456798 knows language Hindi
+  Age: 22 with Name Mark has Phone: 1203456789 knows language Tamil
+  Age: 21 with Name Rutvi has Phone: 1234567890 knows language English
+*/
+// Array Objects
+var arr_names = new Array(4);
+for (var i = 0; i < arr_names.length; i++) {
+    arr_names[i] = i * 2;
+    console.log(">>> Array object", arr_names[i]);
+}
+// Multi-dimensional array
+var multiDimensional = [[1, 2, 3], [23, 24, 25]];
+console.log(multiDimensional[0][0]); // 1
+console.log(multiDimensional[0][1]); // 2
+// Passing Arrays to Functions
+var names = new Array("Mary", "Tom", "Jack", "Jill");
+function display(arr_names) {
+    for (var i = 0; i < arr_names.length; i++) {
+        console.log(">>> Passing arrays to function", names[i]);
+    }
+}
+display(names);
+// Function returning an array
+function disp() {
+    return new Array("Mary", "Tom", "Jack", "Jill");
+}
+var nums = disp();
+console.log(nums);
+// Functions
+function GetStudentsList(students) {
+    students.forEach(function (element) {
+        console.log("Age: " + element.Age + " with Name " + element.Name + " has Phone: " + element.Phone + " knows language " + element.Language);
+    });
+}
+GetStudentsList(studentlist);
+//Rest Parameter
+function GetNumbers() {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    nums.forEach(function (element) {
+        console.log("Number: " + element);
+    });
+}
+GetNumbers(1, 2, 3, 4);
+// Default Parameter
+function GetInfo(info) {
+    if (info === void 0) { info = "Happy"; }
+    console.log(info);
+}
+GetInfo("Very Happy ");
+// Anonymous Function
+var StudentName = function (lName, fName) {
+    return fName + "..." + lName;
+};
+console.log(">>> Anonymous Functions ", StudentName("Patel", "Rutvi"));
+// Arrow Function
+var StudentFullName = function (lName, fName) { return fName + "..." + lName; };
+console.log(StudentFullName("Patel", "Rutvi"));
+// Interfaces
 //# sourceMappingURL=TypeScript.js.map
--- TypeScript.ts
+++ TypeScript.ts
@@ -1,7 +1,8 @@
 console.log("Hello TypeScript!")
 // variable types
 let a = 18;
-console.log(a)  //>>>18
+console.log(a)  
+  //>>>18
 //let a = "Rutvi" 
 // It will show an error. Block scoped variables can't be declared 
 //again with different datatype.
@@ -11,24 +12,90 @@ let d: Number = 100
 // variable with type "any"
 let x;
 x = 100;
-console.log(">>> Any Type variable: x=", x, typeof x);
+console.log(">>> Any Type variable: x=", x, typeof x);  
+  // >>> Any Type variable: x= 100 number
 x = "Rutvi"
-console.log(">>> Any Type variable: x=", x, typeof x);
+console.log(">>> Any Type variable: x=", x, typeof x);  
+  // >>> Any Type variable: x= Rutvi string
+// If number is starting with 0 in typescript, then it will consider that number as an octal number, 
+// except the immediate number after 0 is 8 or 9. 
+// let y = 02.34 // this will also show an error because octal numbers must be integer.
+let y = "02.34";
+let z = parseFloat(y)
+console.log(">>> parse the string as a float",z);  
+  //>>> parse the string as a float 2.34
 // Enum
 enum LanguagesKnown { English, Hindi, Tamil}
 // Arrays
 let students = {
-  Name: "Rutvi Patel",
+  Name: "Rutvi",
   Age: 21,
   Phone: 1234567890,
   Language: LanguagesKnown[LanguagesKnown.English]
 }
 let studentlist = [
-  {Name: "John", Age: 20, Phone: 1023456789, Language: LanguagesKnown[LanguagesKnown.Hindi]},
+  {Name: "John", Age: 20, Phone: 1023456798, Language: LanguagesKnown[LanguagesKnown.Hindi]},
   {Name: "Mark", Age: 22, Phone: 1203456789, Language: LanguagesKnown[LanguagesKnown.Tamil]}
 ]
 studentlist.push(students);
-for (var index = 0; index < studentlist.length; index++) {
-  var element = studentlist[index];
+for (let index = 0; index < studentlist.length; index++) {
+  let element = studentlist[index];
   console.log("Age: " + element.Age + " with Name " + element.Name + " has Phone: " + element.Phone + " knows language " + element.Language);
 }
+/*
+  Age: 20 with Name John has Phone: 1023456798 knows language Hindi
+  Age: 22 with Name Mark has Phone: 1203456789 knows language Tamil
+  Age: 21 with Name Rutvi has Phone: 1234567890 knows language English
+*/
+// Array Objects
+let arr_names:number[] = new Array(4)  
+for(let i = 0;i<arr_names.length;i++) { 
+   arr_names[i] = i * 2 
+   console.log(">>> Array object", arr_names[i]) 
+}
+// Multi-dimensional array
+let multiDimensional:number[][] = [[1,2,3],[23,24,25]]  
+console.log(multiDimensional[0][0])   // 1
+console.log(multiDimensional[0][1])   // 2
+// Passing Arrays to Functions
+let names:string[] = new Array("Mary","Tom","Jack","Jill")  
+function display(arr_names:string[]) {
+   for(let i = 0;i<arr_names.length;i++) { 
+      console.log(">>> Passing arrays to function", names[i]) 
+   }  
+}  
+display(names)
+// Function returning an array
+function disp():string[] { 
+  return new Array("Mary","Tom","Jack","Jill") 
+} 
+let nums:string[] = disp() 
+console.log(nums)
+// Functions
+function GetStudentsList(students: any[]){
+  students.forEach(element => {
+    console.log("Age: " + element.Age + " with Name " + element.Name + " has Phone: " + element.Phone + " knows language " + element.Language);
+  })
+}
+GetStudentsList(studentlist);
+//Rest Parameter
+function GetNumbers(...nums: number[]){
+  nums.forEach(element => {
+    console.log("Number: " + element);
+  });
+}
+GetNumbers(1,2,3,4)
+// Default Parameter
+function GetInfo(info:string = "Happy"){
+  console.log(info);
+}
+GetInfo("Very Happy ")
+// Anonymous Function
+let StudentName = function (lName: string, fName: string) {
+  return fName + "..." + lName;
+}
+console.log(">>> Anonymous Functions ",StudentName("Patel","Rutvi"));
+// Arrow Function
+let StudentFullName = (lName: string, fName: string) => {return fName + "..." + lName};
+console.log(StudentFullName("Patel","Rutvi"));
+// Interfaces
